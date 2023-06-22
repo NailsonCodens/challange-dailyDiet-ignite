02379c5bdedfa28956cceb76f6603f10a2fed96b
Nailson <nailson.ivs@codens.com.br>
2023-06-22 17:15:29 -0300

    update

--- src/routes/meals.routes.ts
+++ src/routes/meals.routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
 import {z} from 'zod';
 import {knex} from '../database';
+import { randomUUID } from "node:crypto";
 
 const mealsRoute = async (app: FastifyInstance) => {
   app.post('/', async(request: FastifyRequest, reply:FastifyReply) => {
@@ -9,13 +10,25 @@ const mealsRoute = async (app: FastifyInstance) => {
       name: z.string().nonempty(),
       description: z.string().nonempty(),
       date: z.string().transform(str => new Date(str)),
-      hour: z.string().refine((value) => /^(?:[01][0-9]|2[0-3]):[0-5][0-9](?::[0-5][0-9])?$/.test(value), 'O formato precisa ser de horas ex: 14:00')
+      hour: z.string().refine((value) => /^(?:[01][0-9]|2[0-3]):[0-5][0-9](?::[0-5][0-9])?$/.test(value), 'O formato precisa ser de horas ex: 14:00'),
+      isInDiet: z.coerce.boolean()
     });  
 
-    requestBodyMeal.parse(request.body);
+    const {name, description, date, hour, isInDiet} = requestBodyMeal.parse(request.body);
+
+    const inDiet = isInDiet === true ? 1 : 0
+
+    //get cookies id user 
+    //do middleware check-user-id
 
     await knex.insert({
-      
+      id: randomUUID(),
+      user_id: /*  */,
+      name,
+      description,
+      date,
+      hour,
+      inDiet
     })
 
 
